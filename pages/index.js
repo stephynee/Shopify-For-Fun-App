d15c426945dbcb26779f5226345d9bc43a2faa42
stephynee <stephykins@gmail.com>
2021-10-07 17:24:24 -0400

    Create homepage component - Renders empty state unless products are found - Uses resource selector to allow users to choose products

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,50 @@
-import { Heading, Page } from "@shopify/polaris";
+import { useState } from 'react';
+import { Page, Card, EmptyState } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import store from 'store-js';
+import ProductList from './Components/ProductList';
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+// Stephanie's learning comments:
+  // App Bridge and Polaris are combined to display a product select on empty state. The resource picker is used to show a list of products that the user can select. The ProductList component is then rendered with a list of the selected products
+  // Local storage used to persist data (Since I'm not using a data base here)
+
+// TODO:
+  // Fix the error where text content does not match between server and client
+
+const Index = () => {
+  const [isResourcePickerOpen, setResourcePickerOpen] = useState(false);
+  const hasProducts = store.get('ids');
+
+  const emptyState = <>
+    <EmptyState
+      heading="Select some products. I don't know what I'm going to do with this yet."
+      image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+      action={{
+        content: "Select products",
+        onAction: () => {
+          setResourcePickerOpen(true);
+        }
+      }}
+    />
+    <ResourcePicker
+      resourceType="Product"
+      open={isResourcePickerOpen}
+      onSelection={(selectPayload) => {
+        const selected = selectPayload.selection.map(product => product.id);
+        setResourcePickerOpen(false);
+        store.set('ids', selected);
+      }}
+      onCancel={() => {
+        setResourcePickerOpen(false);
+      }}
+    />
+  </>;
+
+  return (
+    <Page>
+      <Card>{hasProducts ? (<ProductList />) : emptyState}</Card>
+    </Page>
+  );
+};
 
 export default Index;
